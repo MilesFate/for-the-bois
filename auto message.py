b9a8081050eed9e1371fac46ff5cb2a91d44556b
MilesFate <70919387+MilesFate@users.noreply.github.com>
2020-12-07 20:39:59 -0600

    Add files via upload

---
+++ auto message.py
@@ -0,0 +1,19 @@
+from pip._vendor import requests
+import time
+# https://discord.com/api/v8/channels/750571361646280708/messages test server
+# https://discord.com/api/v8/channels/698337570274279439/messages williams: general
+# https://discord.com/api/v8/channels/754439239625605280/messages william: spam channel
+# https://discord.com/api/v8/channels/764677560667602954/messages kappa pride: bot commands
+
+payload = {
+    'content' : "*Huggie Wuggie big chungus sex lord*"
+}
+
+header = {
+    'authorization': 'NTM4NzMxMDc2MjM1ODIxMDYx.X87bng.iafdz2QqwtjPNo-EKoFlD1En0hU'
+}
+
+while True:
+    r = requests.post('https://discord.com/api/v8/channels/754439239625605280/messages',data= payload,headers=header)
+    r = requests.post('https://discord.com/api/v8/channels/750571361646280708/messages',data= payload,headers=header)
+    time.sleep(300)
