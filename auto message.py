24ed6ccc85e182187d1b986aa91d58a94fa0529f
MilesFate <70919387+MilesFate@users.noreply.github.com>
2020-12-08 09:43:08 -0600

    Update auto message.py

--- auto message.py
+++ auto message.py
@@ -5,14 +5,17 @@ import time
 # https://discord.com/api/v8/channels/754439239625605280/messages william: spam channel
 # https://discord.com/api/v8/channels/764677560667602954/messages kappa pride: bot commands
 
+#this is the message sent, can be text, gifs, and videos
 payload = {
-    'content' : "*Huggie Wuggie big chungus sex lord*"
+    'content' : "*your message here*"
 }
 
+#this lets the script use the account
 header = {
     'authorization': 'account authorization code here'
 }
 
+#this is what sends the message
 i = 0
 while True:
     r = requests.post('https://discord.com/api/v8/channels/754439239625605280/messages',data= payload,headers=header)
