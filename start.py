e24fa3d494b09212677c3299717f051d07bcb567
MilesFate <70919387+MilesFate@users.noreply.github.com>
2020-12-28 11:59:53 -0600

    Add files via upload

---
+++ start.py
@@ -0,0 +1,9 @@
+from backhand import sleep
+def start():
+    teams = "Team options are : tl, c9, tsm, thieves, clg, dig, eg, fly, gg"
+    print('Hello Welcome to my program, I hope you enjoy it! ^_^')
+    print('------------------------------------------------------')
+    sleep()
+    print("Choose an LCS team who's roaster you wish to see")
+    print(teams)
+
