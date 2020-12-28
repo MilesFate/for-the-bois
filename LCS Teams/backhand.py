fa0096b73eb4f53adbdec10d194b6fd67f17097f
MilesFate <70919387+MilesFate@users.noreply.github.com>
2020-12-28 11:59:55 -0600

    Add files via upload

---
+++ LCS Teams/backhand.py
@@ -0,0 +1,194 @@
+import time
+#this is the template
+'''
+def ():
+    class :
+        Top=''
+        Jungle=''
+        Mid=''
+        Bot=''
+        Sup =''
+    =
+    print(lines)
+    print("This is 's Current Roaster for LCS.")
+    print(lines)
+    sleep()
+    print("In the Top Lane "+ .Top)
+    print("In the Jungle "+ .Jungle)
+    print("In the Mid Lane "+ .Mid)
+    print("In the Bot Lane "+ .Bot)
+    print("In the Support Role "+ .Sup)
+'''
+lines= '---------------------------------'
+
+def lap():
+        sleep()
+        teams = "Team options are : tl, c9, tsm, thieves, clg, dig, eg, fly, gg"
+        print(lines)
+        print(teams)
+
+def sleep():
+    time.sleep(1)
+
+def Team_Liquid():
+    class Team_Liquid:
+        Top='Alphari'
+        Jungle='Santorin'
+        Mid='Jenson'
+        Bot='Tactical'
+        Sup ='CoreJJ'
+    tl = Team_Liquid
+    print(lines)
+    print("This is Team Liquid's Current Roaster for LCS.")
+    print(lines)
+    sleep()
+    print("In the Top Lane "+ tl.Top)
+    print("In the Jungle "+ tl.Jungle)
+    print("In the Mid Lane "+ tl.Mid)
+    print("In the Bot Lane "+ tl.Bot)
+    print("In the Support Role "+ tl.Sup)
+    
+def Cloud_9():
+    class Cloud_9:
+        Top='Fudge'
+        Jungle='Blaber'
+        Mid='Perkz'
+        Bot='Zven'
+        Sup ='Vulcan'
+    c9=Cloud_9
+    print(lines)
+    print("This is Cloud 9's Current Roaster for LCS.")
+    print(lines)
+    sleep()
+    print("In the Top Lane "+ c9.Top)
+    print("In the Jungle "+ c9.Jungle)
+    print("In the Mid Lane "+ c9.Mid)
+    print("In the Bot Lane "+ c9.Bot)
+    print("In the Support Role "+ c9.Sup)
+
+def TSM():
+    class TSM:
+        Top='Huni'
+        Jungle='Spica'
+        Mid='Power Of Evil'
+        Bot='Lost'
+        Sup ='SwordArt'
+    tsm = TSM
+    print(lines)
+    print("This is TSM's Current Roaster for LCS.")
+    print(lines)
+    sleep()
+    print("In the Top Lane "+ tsm.Top)
+    print("In the Jungle "+ tsm.Jungle)
+    print("In the Mid Lane "+ tsm.Mid)
+    print("In the Bot Lane "+ tsm.Bot)
+    print("In the Support Role "+ tsm.Sup)
+
+def Thieves():
+    class Thieves:
+        Top='Ssumday'
+        Jungle='Closer'
+        Mid='Damonte'
+        Bot='FBI'
+        Sup ='huhi'
+    thieves=Thieves
+    print(lines)
+    print("This is Thieves's Current Roaster for LCS.")
+    print(lines)
+    sleep()
+    print("In the Top Lane "+ thieves.Top)
+    print("In the Jungle "+ thieves.Jungle)
+    print("In the Mid Lane "+ thieves.Mid)
+    print("In the Bot Lane "+ thieves.Bot)
+    print("In the Support Role "+ thieves.Sup)
+
+def CLG():
+    class CLG:
+        Top='Finn'
+        Jungle='Broxah'
+        Mid='Pobelter'
+        Bot='WildTurtle'
+        Sup ='Smoothie'
+    clg=CLG
+    print(lines)
+    print("This is CLG's Current Roaster for LCS.")
+    print(lines)
+    sleep()
+    print("In the Top Lane "+ clg.Top)
+    print("In the Jungle "+ clg.Jungle)
+    print("In the Mid Lane "+ clg.Mid)
+    print("In the Bot Lane "+ clg.Bot)
+    print("In the Support Role "+ clg.Sup)
+
+def Dignitas():
+    class Dignitas:
+        Top='FakeGod'
+        Jungle='Dardoch'
+        Mid='Soligo'
+        Bot='Neo'
+        Sup ='aphromoo'
+    dig=Dignitas
+    print(lines)
+    print("This is Dignitas's Current Roaster for LCS.")
+    print(lines)
+    sleep()
+    print("In the Top Lane "+ dig.Top)
+    print("In the Jungle "+ dig.Jungle)
+    print("In the Mid Lane "+ dig.Mid)
+    print("In the Bot Lane "+ dig.Bot)
+    print("In the Support Role "+ dig.Sup)
+
+def Evil_Geniuses():
+    class Evil_Geniuses:
+        Top='Impact'
+        Jungle='Svenskeren'
+        Mid='Jiizuke'
+        Bot='Deftly'
+        Sup ='IgNar'
+    eg=Evil_Geniuses
+    print(lines)
+    print("This is Evil Geniuses's Current Roaster for LCS.")
+    print(lines)
+    sleep()
+    print("In the Top Lane "+ eg.Top)
+    print("In the Jungle "+ eg.Jungle)
+    print("In the Mid Lane "+ eg.Mid)
+    print("In the Bot Lane "+ eg.Bot)
+    print("In the Support Role "+ eg.Sup)
+
+def Fly_Quest():
+    class Fly_Quest:
+        Top='Licorice'
+        Jungle='Josedeodo'
+        Mid='Palafox'
+        Bot='Johnsun'
+        Sup ='Diamond'
+    fly=Fly_Quest
+    print(lines)
+    print("This is Fly Quest's Current Roaster for LCS.")
+    print(lines)
+    sleep()
+    print("In the Top Lane "+ fly.Top)
+    print("In the Jungle "+ fly.Jungle)
+    print("In the Mid Lane "+ fly.Mid)
+    print("In the Bot Lane "+ fly.Bot)
+    print("In the Support Role "+ fly.Sup)
+
+def Golden_Guardians():
+    class Golden_Guardians:
+        Top='Niles'
+        Jungle='Iconic'
+        Mid='Ablazeolive'
+        Bot='Stixxay'
+        Sup ='Newbie'
+    gg=Golden_Guardians
+    print(lines)
+    print("This is Golden Guardians's Current Roaster for LCS.")
+    print(lines)
+    sleep()
+    print("In the Top Lane "+ gg.Top)
+    print("In the Jungle "+ gg.Jungle)
+    print("In the Mid Lane "+ gg.Mid)
+    print("In the Bot Lane "+ gg.Bot)
+    print("In the Support Role "+ gg.Sup)
+
