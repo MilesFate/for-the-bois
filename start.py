aa8298037df21884f0115e70421c11aec4a4eab2
MilesFate <70919387+MilesFate@users.noreply.github.com>
2020-12-28 12:00:20 -0600

    Delete start.py

--- start.py
+++
@@ -1,9 +0,0 @@
-from backhand import sleep
-def start():
-    teams = "Team options are : tl, c9, tsm, thieves, clg, dig, eg, fly, gg"
-    print('Hello Welcome to my program, I hope you enjoy it! ^_^')
-    print('------------------------------------------------------')
-    sleep()
-    print("Choose an LCS team who's roaster you wish to see")
-    print(teams)
-
