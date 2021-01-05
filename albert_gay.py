cab06cb6951dce21a9e21ff543e01107450c5642
MilesFate <70919387+MilesFate@users.noreply.github.com>
2021-01-04 20:48:43 -0600

    Add files via upload

---
+++ albert_gay.py
@@ -0,0 +1,15 @@
+import time
+def slump():
+    time.sleep(4)
+def gay():
+        print("-----------             /|    \       /")
+        print("|                      / |     \     /")
+        print("|                     /  |      \   /")
+        print("|     -----|         /___|       \ /")
+        print("|          |        /    |        |")
+        print("|          |       /     |        |")
+        print("-----------|      /      |        |")
+while True:
+    gay()
+    slump()
+    
